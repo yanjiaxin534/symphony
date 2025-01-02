916e1a979e7a1007bdf7b14b27fe814c67bdd848
Jiaxin Yan <jiaxinyan@microsoft.com>
2025-01-02 13:50:18 +0800

    add remove

--- api/pkg/apis/v1alpha1/vendors/stage-vendor.go
+++ api/pkg/apis/v1alpha1/vendors/stage-vendor.go
@@ -402,6 +402,7 @@ func (s *StageVendor) Init(config vendors.VendorConfig, factories []managers.IMa
 				MergedState:          planEnvelope.MergedState,
 				Deployment:           planEnvelope.Deployment,
 				Namespace:            planEnvelope.Namespace,
+				Remove:               planEnvelope.Remove,
 				TargetResult:         make(map[string]int),
 				CurrentState:         planEnvelope.CurrentState,
 				StepStates:           make([]StepState, len(planEnvelope.Plan.Steps)),
@@ -512,6 +513,7 @@ func (s *StageVendor) saveSummaryAndPlanState(ctx context.Context, planState *Pl
 		log.InfoCtx(ctx, " update phase getr %v ", stepResult.retComoponents)
 		log.InfoCtx(ctx, " update phase getc %v ", stepResult.Components)
 		planState.StepStates[stepResult.StepId].GetResult = stepResult.retComoponents
+
 	case PhaseApply:
 		log.InfoCtx(ctx, "todo apply plan state components c%+v", stepResult.Components)
 		log.InfoCtx(ctx, "todo apply plan state componentsr %+v", stepResult.retComoponents)
@@ -659,7 +661,9 @@ func (s *StageVendor) handlePhaseGetCompletetion(ctx context.Context, planState 
 		}
 		log.InfoCtx(ctx, "get desired state %+v", desiredState)
 		if planState.Remove {
+			log.InfoCtx(ctx, "it is removed mark all as removed")
 			desiredState.MarkRemoveAll()
+			log.InfoCtx(ctx, "after remove desired state %+v", desiredState)
 		}
 
 		mergedState := solution.MergeDeploymentStates(&currentState, desiredState)
